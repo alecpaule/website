28be595174cee4c427ba6ea88512113d53de7238
Alexander Paule <alec.paule@yahoo.com>
2021-11-28 23:00:59 -0800

    Adds Hello section and colors/shadow

---
+++ src/Hello.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Hello.scss";
+
+class Hello extends Component {
+  render() {
+    return (
+      <section>
+        <h1>Alec Paule</h1>
+        <p className="bio">Hello! My name is Alec and this is my portfolio.</p>
+      </section>
+    );
+  }
+}
+
+export default Hello;
