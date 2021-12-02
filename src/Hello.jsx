825fc62969a395dce4921799a905913f12a2b944
Alexander Paule <alec.paule@yahoo.com>
2021-12-01 16:41:09 -0800

    Changes color and text style

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,13 +1,42 @@
 import React, { Component } from "react";
 import "./Hello.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faGithub,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
 class Hello extends Component {
   render() {
     return (
-      <section>
+      <div className="hello">
         <h1>Alec Paule</h1>
-        <p className="bio">Hello! My name is Alec and this is my portfolio.</p>
-      </section>
+
+        <div className="links">
+          <a
+            className="linkedin"
+            href="https://www.linkedin.com/in/alexander-paule-37600ab8/"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          </a>
+          <a
+            className="github"
+            href="https://github.com/alecpaule"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
+          <a
+            className="neko"
+            href="https://www.instagram.com/softboiledneko/"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
+          </a>
+        </div>
+      </div>
     );
   }
 }
